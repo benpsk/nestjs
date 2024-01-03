83099825c7b22bd5c9dc2a903bd917b7ee5a956b
Ben Gunn <bengunn.dev@gmail.com>
2024-01-03 21:43:34 +0630

    nestjs: overview

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { CatsController } from './cats/cats.controller';
+import { logger } from './common/middleware/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(logger).forRoutes(CatsController);
+  }
+}
